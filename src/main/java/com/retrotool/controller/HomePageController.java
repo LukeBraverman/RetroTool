640cf60979d850ee74157d307253c83d17b16e40
lbrav <l.braverman.l@live.co.uk>
2021-12-20 01:38:22 +0000

    Written implementation for header component.

--- src/main/java/com/retrotool/controller/HomePageController.java
+++ src/main/java/com/retrotool/controller/HomePageController.java
@@ -1,14 +1,18 @@
 package com.retrotool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
 
     @GetMapping("/")
-    public String HomePage() {
+    public String HomePage(Model model) {
 
+        String headerText = "RetroTool!";
+
+        model.addAttribute("headerText", headerText);
 
 
         return "HomePage";
