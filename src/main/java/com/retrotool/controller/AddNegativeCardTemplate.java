338a6711218adddf5d2b1645636601eb1f54e05f
lbrav <l.braverman.l@live.co.uk>
2022-01-03 00:35:39 +0000

    Added negative and neautral controllers. Need to add tests

---
+++ src/main/java/com/retrotool/controller/AddNegativeCardTemplate.java
@@ -0,0 +1,13 @@
+package com.retrotool.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class AddNegativeCardTemplate {
+
+    private HomePageService homePageService;
+
+
+}
---
+++ src/main/java/com/retrotool/controller/AddNeutralCardTemplate.java
@@ -0,0 +1,14 @@
+package com.retrotool.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class AddNeutralCardTemplate {
+
+    private HomePageService homePageService;
+    /*
+   todo: add tests then implement code for neutral and negative controllers. Essentiall a copy and past of add positive controlller
+     */
+}
